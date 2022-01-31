21e6138295e288a93b4f9c27024451b34f17581f
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2022-01-31 08:29:40 -0500

    Remove unused import

--- test/dataset_processor_test.exs
+++ test/dataset_processor_test.exs
@@ -2,10 +2,9 @@ defmodule DatasetTest do
   use ExUnit.Case
   doctest Dataset
 
-  import ExUnit.CaptureLog
   require Logger
 
-  test "test logger" do
+  test "no such file or directory" do
     assert catch_error(Dataset.parse_csv("dummy", ";"))
   end
 end
