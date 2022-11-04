25d53aeb54c26e41b58c1f5aee3e85265ea2ffa0
JuanDGiraldoM <juandagiraldoma@gmail.com>
2022-11-04 11:30:15 -0500

    Change elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PerfAnalyzer.MixProject do
   def project do
     [
       app: :perf_analyzer,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       escript: [
@@ -25,13 +25,13 @@ defmodule PerfAnalyzer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:castore, "~> 0.1.15"},
+      {:castore, "~> 0.1.18"},
       {:mint, "~> 1.4"},
       {:httpoison, "~> 1.8"},
       {:poison, "~> 5.0"},
       {:distillery, "~> 2.1"},
       {:nimble_csv, "~> 1.2"},
-      {:file_size, "~> 3.0"}
+      {:file_size, "~> 3.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
