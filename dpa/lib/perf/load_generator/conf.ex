dbb7b6dd806171549f7e4d2d50880e0bd459625b
JuanDGiraldoM <juandagiraldoma@gmail.com>
2023-05-11 16:17:06 -0500

    Remove unused files

--- dpa/lib/perf/load_generator/conf.ex
+++
@@ -1,9 +0,0 @@
-defmodule Perf.LoadGenerator.Conf do
-
-  defstruct [:method, :path, :headers, :body]
-
-  def new(method, path) do
-    %Perf.Model.Request{method: method, path: path, headers: [], body: ""}
-  end
-
-end
--- dpa/lib/perf/model/request.ex
+++
@@ -1,6 +0,0 @@
-defmodule Perf.Model.Request do
-  @moduledoc false
-
-  defstruct [:method, :path, :headers, :body, :url, :item]
-
-end
