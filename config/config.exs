ad46a546de0379e6003d181471efd7473fe398cc
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-06-01 11:01:33 -0500

    Change hooks env

--- config/config.exs
+++ config/config.exs
@@ -12,4 +12,28 @@ config :distributed_performance_analyzer,
   dataset_parser: DistributedPerformanceAnalyzer.Infrastructure.Adapters.FileSystem.Parser,
   report_csv: DistributedPerformanceAnalyzer.Infrastructure.Adapters.OutputCsv
 
+if Mix.env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    hooks: [
+      pre_commit: [
+        verbose: true,
+        tasks: [
+          {:file, "./hooks/mix_format"},
+          {:mix_task, :format, ["--check-formatted", "--dry-run"]},
+          {:mix_task, :test, ["--color", "--cover"]},
+          {:mix_task, :credo,
+           [
+             "--sonarqube-base-folder",
+             "./",
+             "--sonarqube-file",
+             "credo_sonarqube.json",
+             "--mute-exit-status"
+           ]},
+          {:mix_task, :sobelow}
+        ]
+      ]
+    ]
+end
+
 import_config "#{Mix.env()}.exs"
