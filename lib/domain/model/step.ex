89db3657838d8696bf1cad8f196ef53c80358d8d
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-07-28 13:08:53 -0500

    Improve strings params in construct

--- lib/domain/model/step.ex
+++ lib/domain/model/step.ex
@@ -8,7 +8,7 @@ defmodule DistributedPerformanceAnalyzer.Domain.Model.Step do
 
   constructor do
     field(:execution_model, ExecutionModel.t(), enforce: true)
-    field(:name, String.t(), constructor: &is_string/1)
+    field(:name, :string, constructor: &is_string/1)
     field(:step_number, :integer, constructor: &is_integer/1, enforce: true)
     field(:concurrency, :integer, constructor: &is_integer/1)
   end
