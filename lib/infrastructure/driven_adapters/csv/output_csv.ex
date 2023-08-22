a4c3bce6d93e2c8bdba78bf8d2530d7ecea3ee6b
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-08-22 16:33:11 -0500

    Fix report headers

--- lib/infrastructure/driven_adapters/csv/output_csv.ex
+++ lib/infrastructure/driven_adapters/csv/output_csv.ex
@@ -2,7 +2,6 @@ defmodule DistributedPerformanceAnalyzer.Infrastructure.Adapters.OutputCsv do
   @moduledoc """
   Print outgoing report file in csv format
   """
-  alias DistributedPerformanceAnalyzer.Utils.DataTypeUtils
   require Logger
 
   @spec save_csv(any(), String.t(), String.t(), boolean()) :: {:ok}
@@ -16,14 +15,18 @@ defmodule DistributedPerformanceAnalyzer.Infrastructure.Adapters.OutputCsv do
 
     IO.binwrite(file, header <> "\n")
 
-    data
-    |> Stream.map(fn row ->
-      if print do
-        IO.puts(row)
-      end
+    rows =
+      data
+      |> Stream.map(fn row ->
+        if print do
+          IO.puts(row)
+        end
 
-      row <> "\n"
-    end)
+        row <> "\n"
+      end)
+
+    [header <> "\n"]
+    |> Stream.concat(rows)
     |> Stream.into(File.stream!(file_name))
     |> Stream.run()
   end
