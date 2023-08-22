987d4cc6426dd8dd30f01c78eab6d890654bae13
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-08-22 16:33:11 -0500

    Fix report headers

--- lib/infrastructure/driven_adapters/csv/output_csv.ex
+++ lib/infrastructure/driven_adapters/csv/output_csv.ex
@@ -2,28 +2,27 @@ defmodule DistributedPerformanceAnalyzer.Infrastructure.Adapters.OutputCsv do
   @moduledoc """
   Print outgoing report file in csv format
   """
-  alias DistributedPerformanceAnalyzer.Utils.DataTypeUtils
   require Logger
 
   @spec save_csv(any(), String.t(), String.t(), boolean()) :: {:ok}
   def save_csv(data, file_name, header, print) do
-    {:ok, file} = File.open(file_name, [:write])
-
     if print do
       IO.puts("####CSV#######")
       IO.puts(header)
     end
 
-    IO.binwrite(file, header <> "\n")
+    rows =
+      data
+      |> Stream.map(fn row ->
+        if print do
+          IO.puts(row)
+        end
 
-    data
-    |> Stream.map(fn row ->
-      if print do
-        IO.puts(row)
-      end
+        row <> "\n"
+      end)
 
-      row <> "\n"
-    end)
+    [header <> "\n"]
+    |> Stream.concat(rows)
     |> Stream.into(File.stream!(file_name))
     |> Stream.run()
   end
