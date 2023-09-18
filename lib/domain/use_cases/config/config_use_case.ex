902eeb5b404139856e4989a2bbd5d01a30402036
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-09-18 12:31:56 -0500

    Identify config file version

--- lib/domain/use_cases/config/config_use_case.ex
+++ lib/domain/use_cases/config/config_use_case.ex
@@ -5,40 +5,59 @@ defmodule DistributedPerformanceAnalyzer.Domain.UseCase.Config.ConfigUseCase do
   require Logger
 
   def parse_config_file(application_envs) do
-    url = application_envs[:url]
     distributed = application_envs[:distributed]
-    execution = application_envs[:execution]
-    request = application_envs[:request]
     jmeter_report = application_envs[:jmeter_report]
 
     Logger.info("JMeter Report enabled: #{jmeter_report}")
 
-    %{
-      host: host,
-      path: path,
-      scheme: scheme,
-      port: port,
-      query: query
-    } = DataTypeUtils.parse(url)
+    case identify_config_file_version(application_envs) do
+      2 ->
+        requests = application_envs[:requests]
+        datasets = application_envs[:datasets]
+        strategies = application_envs[:strategies]
+        scenarios = application_envs[:scenarios]
 
-    connection_conf = {scheme, host, port}
+      1 ->
+        url = application_envs[:url]
+        execution = application_envs[:execution]
+        request = application_envs[:request]
 
-    {:ok, request_conf} =
-      request
-      |> Map.put(:path, DataTypeUtils.path(path, query))
-      |> Map.put(:url, url)
-      |> Request.new()
+        %{
+          host: host,
+          path: path,
+          scheme: scheme,
+          port: port,
+          query: query
+        } = DataTypeUtils.parse(url)
 
-    {:ok, execution_conf} =
-      execution
-      |> Map.put(:request, request_conf)
-      |> ExecutionModel.new()
+        connection_conf = {scheme, host, port}
 
-    {:ok,
-     %{
-       distributed: distributed,
-       connection_conf: connection_conf,
-       execution_conf: execution_conf
-     }}
+        {:ok, request_conf} =
+          request
+          |> Map.put(:path, DataTypeUtils.path(path, query))
+          |> Map.put(:url, url)
+          |> Request.new()
+
+        {:ok, execution_conf} =
+          execution
+          |> Map.put(:request, request_conf)
+          |> ExecutionModel.new()
+
+        {:ok,
+         %{
+           distributed: distributed,
+           connection_conf: connection_conf,
+           execution_conf: execution_conf
+         }}
+    end
+  end
+
+  defp identify_config_file_version(env) do
+    if env[:requests] &&
+         env[:datasets] &&
+         env[:strategies] &&
+         env[:scenarios],
+       do: 2,
+       else: 1
   end
 end
