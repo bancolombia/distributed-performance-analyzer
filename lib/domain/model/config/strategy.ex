4e169e9f6d3f46c677f8975c33a90bbe7e3cc82c
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-12-01 10:24:41 -0500

    Add initial pool size to strategy model

--- lib/domain/model/config/strategy.ex
+++ lib/domain/model/config/strategy.ex
@@ -8,6 +8,7 @@ defmodule DistributedPerformanceAnalyzer.Domain.Model.Config.Strategy do
   constructor do
     field(:steps, :integer, constructor: &is_integer/1)
     field(:duration, :integer, constructor: &is_integer/1)
+    field(:initial, :integer, constructor: &is_integer/1, default: 0)
     field(:increment, :integer, constructor: &is_integer/1)
     field(:constant_load, :boolean, constructor: &is_boolean/1, default: false)
   end
