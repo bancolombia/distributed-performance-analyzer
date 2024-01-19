a5ae212dc290ff3c8c58d3584e7b41c5da0d2add
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2024-01-19 12:19:51 -0500

    Update model for request

--- lib/domain/model/config/request.ex
+++ lib/domain/model/config/request.ex
@@ -11,6 +11,7 @@ defmodule DistributedPerformanceAnalyzer.Domain.Model.Config.Request do
     field(:headers, :lists, constructor: &is_list/1)
     field(:body, :any)
     field(:params, :lists, constructor: &is_list/1, default: [], enforce: false)
-    field(:timeout, :integer, constructor: &is_integer/1, default: 30, enforce: false)
+    field(:timeout, :integer, constructor: &is_integer/1, default: 100_000, enforce: false)
+    field(:ssl, :boolean, constructor: &is_boolean/1, default: true, enforce: false)
   end
 end
--- lib/domain/model/config/response.ex
+++ lib/domain/model/config/response.ex
@@ -7,7 +7,9 @@ defmodule DistributedPerformanceAnalyzer.Domain.Model.Config.Response do
 
   constructor do
     field(:status, :integer, constructor: &is_integer/1)
-    field(:message, :integer, constructor: &is_integer/1)
+    field(:message, String.t(), constructor: &is_string/1)
     field(:headers, :list, constructor: &is_list/1)
+    field(:time, :integer, constructor: &is_integer/1)
+    field(:connection_time, :integer, constructor: &is_integer/1)
   end
 end
